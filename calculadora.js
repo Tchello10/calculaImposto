e2465f15b6255bea9af34ec4b87da3e45155f6a9
marcelusos10 <marcelofersto@gmail.com>
2023-01-17 16:54:31 -0300

    Criados os campos e testes de JS

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,24 @@
 console.log("Funcionou o JS");
+
+let inputValor = document.getElementById("valorCota");
+let inputQtd = document.getElementById("qtdCota");
+let resultado = document.getElementById("testOutput");
+
+let timeout = null;
+
+if (inputQtd != null || inputValor != null) {
+  inputQtd.addEventListener("keyup", function (e) {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(function () {
+      console.log("Input Value: ", valorCota.value * qtdCota.value);
+    }, 1000);
+  });
+  inputValor.addEventListener("keyup", function (e) {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(function () {
+      resultado.innerHTML("Input Value: ", valorCota.value * qtdCota.value);
+    }, 1000);
+  });
+}
